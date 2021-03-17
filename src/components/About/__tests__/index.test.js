a9bb906a21def9e0baf8ec1dfc0d545a0ec6c4c4
Joe Rehfuss <joe.rehfuss@gmail.com>
2021-03-17 16:26:27 -0700

    why

---
+++ src/components/About/__tests__/index.test.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {render, cleanup} from "@testing-library/react";
+import '@testing-library/jest-dom/extend-expect';
+import About from "../"
+
+afterEach(cleanup);
+
+describe("about",()=>{
+    it('renders',()=>{
+        render(<About/>)
+    })
+    it('matches snapshot DOM node structure', () => {
+        // render About
+        const { asFragment } = render(<About />);
+        expect(asFragment()).toMatchSnapshot();
+      })
+})
